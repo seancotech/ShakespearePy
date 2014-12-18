3e3028949e6180bc41a11622137069f9adf65b37
Sean Collins <scollins36@gatech.edu>
2014-12-17 23:11:18 -0500

    Complete method to return whole speech from search string

--- Newspeech.py
+++ Newspeech.py
@@ -1,20 +1,38 @@
 # Used to encode search returns
 from ShakespeareSpeech import ShakespeareSpeech
 
+# Tags to search for
+PLAY_TAG = "<PLAY>"
+SPEAKER_TAG = "<SPEAKER>"
+SPEECH_TAG = "<SPEECH>"
+OPEN_TAG = "<"
+CLOSE_TAG = ">"
+
 class Newspeech(object):
     """Stores an XML play or speech file."""
 
     def __init__(self, body):
         self.body = body
 
-    def getSpeech(searchString):
+    def getSpeech(self, searchString):
         """Returns a ShakespeareSpeech object with the relevant speech 
         corresponding to the given search string."""
 
+        index = -1
 
-    def backtrack(index, tag):
+        try:
+            index = self.body.index(searchString)
+        except ValueError:
+            # The string wasn't found in any play!
+            return False
+
+        play = backtrack(index, PLAY_TAG)
+        speaker = backtrack(index, SPEAKER_TAG)
+
+
+    def backtrack(self, index, tag):
         """Backtracks from the provided index until the given tag is encountered.
-        Returns the text at that tag."""
+        Returns the text at that tag. For finding a speech body, use backtrackGetSpeech() instead."""
 
         # Split the body up to the index
         splitBody = self.body[0:index]
@@ -29,4 +47,20 @@ class Newspeech(object):
         # Return contents
         return splitBody[startIndex:nextOpenTagIndex]
 
+    def backtrackGetSpeech(self, index):
+        """Backtracks from the provided speech index and returns the full speech."""
 
+        startSpeechIndex = getPreviousIndex(self.body, CLOSE_TAG, index) + 1
+        endSpeechIndex = getNextIndex(self.body, OPEN_TAG, index)
+
+        return self.body[startSpeechIndex:endSpeechIndex]
+
+    def getPreviousIndex(haystack, needle, index):
+        """Returns the previous index of a substring ending at the current index in haystack."""
+
+        return haystack.rfind(needle, 0, index)
+        
+    def getNextIndex(haystack, needle, index):
+        """Returns the next index of a substring starting from the current index in haystack."""
+
+        return haystack.find(needle, index + 1)
